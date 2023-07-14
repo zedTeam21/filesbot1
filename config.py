1f283ed72602e7ac10830ae442d0694572d944a5
Techlegacy7 <99082897+Techlegacy7@users.noreply.github.com>
2023-07-14 12:29:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-#(©)CodeXBotz
+
 
 
 
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6303422917:AAGVRS85j3nZQrYddgAKMIj1WcTD4mceiwc")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6379813085:AAGnV2PayXJrkulNet2-UF70nI1JjLLX_UU")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "25354498"))
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001254017219"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "👋 Hello {first}! Welcome to the Anime Bot! \n\n🌟I'm here to help you watch any anime you desire. Join our main channel @iAnimeHub for more! 📺🌸.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join my Channel in order to use me\n\nKindly join @iAnimeHub and it\'s private channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -58,10 +58,11 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "Hey {first} \n\nPlease do not send me any direct message as I can only send you files once you click on any link.\n\nJoin @iAnimeHub for more anime fun! 🌟🌸 Explore a wide variety of shows and connect with fellow enthusiasts. "
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1969177696)
+ADMINS.append(163343128)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
