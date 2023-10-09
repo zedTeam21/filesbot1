f54f9df69cedf4bd48f244ce2099df7267b41883
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2023-10-09 12:33:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+
+# Create a Flask application instance
 app = Flask(__name__)
 
+# Define a route for the root URL ('/')
 @app.route('/')
 def hello_world():
-    return 'GreyMatters'
-
+    # This function is executed when someone accesses the root URL
+    return 'GreyMatters'  # Return the string 'GreyMatters' as the response
 
 if __name__ == "__main__":
+    # Start the Flask development server if this script is executed directly
     app.run()
